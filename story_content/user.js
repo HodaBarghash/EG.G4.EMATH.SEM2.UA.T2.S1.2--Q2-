173a04ab78d0b74320ae82c13f802786b2f0908d
HodaBarghash <121548764+HodaBarghash@users.noreply.github.com>
2023-02-02 19:27:35 +0200

    g4

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,26 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "5vi8GkLYHpF":
+        Script1();
+        break;
   }
+}
+
+function Script1()
+{
+  const url="https://script.google.com/macros/s/AKfycbzS9vE7zkcZ4bsWvQN2cELAVmy73kURuoItzvrAWpAdA-OeSKrt4SpFX__9H21Gc5096A/exec"
+
+var player=GetPlayer()
+
+fetch(url,{
+	method:"POST",
+	mode: 'no-cors',
+	cache:'no-cache',
+	headers: {'Content-Type': 'application/json'},
+	redirect:'follow',
+	body: JSON.stringify({q1attempts:player.GetVar("q1attempts"),q1answeredcorrect:player.GetVar("q1answeredcorrect")})
+	}
+	)
 }
 
